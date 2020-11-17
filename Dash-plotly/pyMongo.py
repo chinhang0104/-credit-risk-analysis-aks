399ae117dfc9d09df3835be01cd04fcb448b968e
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2020-11-17 14:33:45 +0800

    Update pyMongo.py

--- Dash-plotly/pyMongo.py
+++ Dash-plotly/pyMongo.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import pprint as pp
 
+# change you key to the correct one
 URL_Mongo = 'mongodb://Msbd5003-mongodb:zsETiWDaAJdqODfOg62miplS9kHMEs5N5xhmgyIzSzEaOKVfMdJp7IUThBqd7obK1OBsnR1n2hHqMdORMCCx6w==@msbd5003-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@msbd5003-mongodb@'
 client = MongoClient(URL_Mongo)
 
